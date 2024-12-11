410e82889aff73ec2969b23b3a2ad3133b665ed1
Mridul24agaga <mridulthareja8@gmail.com>
2024-12-11 19:21:52 +0530

    mriods

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "@/app/components/Hero"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <div>
       <Hero/>
+      <Analytics/>
     </div>
   );
 }
