ee06ed1bdfc0fc1b9e817385e5a785db769ca5be
Mridul24agaga <mridulthareja8@gmail.com>
2024-12-14 17:24:43 +0530

    shuffl

--- app/directory-submit-75$162907612/page.tsx
+++ app/directory-submit-75$162907612/page.tsx
@@ -91,7 +91,7 @@ export default function DirectoryListing() {
         </div>
         <button
           onClick={selectRandomDirectories}
-          className="px-4 py-2 bg-orange-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 flex items-center"
+          className="px-4 py-2 bg-orange-500 text-white font-semibold rounded-lg shadow-md hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 flex items-center"
         >
           <Shuffle className="w-5 h-5 mr-2" />
           Random Selection
